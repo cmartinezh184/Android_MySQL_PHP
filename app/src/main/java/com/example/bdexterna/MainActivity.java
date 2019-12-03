5380889cf1afdec5d2d20f221b7ea861247f7c6c
Cristhian Martínez <cmartinezh2623@hotmail.com>
2019-12-03 01:04:28 -0600

    Cambios para el CRUD de articulos y navegacion a la pantalla de agregar imagenes.

--- app/src/main/java/com/example/bdexterna/MainActivity.java
+++ app/src/main/java/com/example/bdexterna/MainActivity.java
@@ -1,30 +1,40 @@
 package com.example.bdexterna;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText codigo, descripcion;
-    private Button agregar, modificar, eliminar, enlistar;
+    private Button agregar, modificar, eliminar, subirImagen, enlistar;
     private ListView listaProductos;
+    private final String url = "http://192.168.100.44/inventarios/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,40 +47,60 @@ public class MainActivity extends AppCompatActivity {
         modificar = findViewById(R.id.btn_modificar);
         eliminar = findViewById(R.id.btn_eliminar);
         enlistar = findViewById(R.id.btn_enlistar);
+        subirImagen = findViewById(R.id.btn_adjuntar_foto);
         listaProductos = findViewById(R.id.listaProductos);
+
+        cargarLista();
 
         agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConectarServicio("http://192.168.100.44/inventarios/insertar.php?codigo=" +
-                        codigo.getText().toString() + "&descripcion=" + descripcion.getText().toString());
+                ConectarServicio("insertar.php?codigo=" + codigo.getText().toString() +
+                        "&descripcion=" + descripcion.getText().toString());
+                codigo.setText("");
+                descripcion.setText("");
+                cargarLista();
             }
         });
 
         modificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConectarServicio("http://192.168.100.44/inventarios/modificar.php");
+                ConectarServicio("modificar.php?codigo=" + codigo.getText().toString() +
+                        "&descripcion=" + descripcion.getText().toString());
+                codigo.setText("");
+                descripcion.setText("");
+                cargarLista();
             }
         });
 
         eliminar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConectarServicio("http://192.168.100.44/inventarios/eliminar.php");
+                ConectarServicio("eliminar.php?codigo=" + codigo.getText().toString());
+                codigo.setText("");
+                descripcion.setText("");
+                cargarLista();
             }
         });
 
         enlistar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ConectarServicio("http://192.168.100.44/inventarios/enlistar.php");
+                ConectarServicio("enlistar.php");
+            }
+        });
+
+        subirImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, UploadImageActivity.class));
             }
         });
     }
 
-    private void ConectarServicio(String url){
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+    private void ConectarServicio(String q){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url + q, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
@@ -93,4 +123,42 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.start();
         requestQueue.add(stringRequest);
     }
+
+    private boolean cargarLista(){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final ArrayList<String> lista = new ArrayList<>();
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url + "enlistar.php", null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject object = null;
+                    try {
+                        object = response.getJSONObject(i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    String objectString = object.toString();
+                    lista.add(objectString);
+                }
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", "Error response: ", error);
+            }
+        });
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lista);
+        listaProductos.setAdapter(arrayAdapter);
+        arrayAdapter.notifyDataSetChanged();
+
+        queue.add(jsonArrayRequest);
+
+        return true;
+    }
+
 }
